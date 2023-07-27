fc7077b4209a751528a6c3536e07f4a317fc4884
Agnes-Kalunda <agypeter97@gmail.com>
2023-07-27 16:53:24 +0300

    configured openai key in my project

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import { Configuration, OPenAIAPI } from "openai"
 function App() {
   const [prompt, setPrompt] = useState("")
 
+  const configuration = new Configuration({
+    apikey: import.meta.env.VITE_Open_AI_Key
+})
+
   return (
     <>
      <h2>Hiiiiii</h2>
